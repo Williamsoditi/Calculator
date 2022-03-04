2871bf72067417d84f509a67629dde92378bb4f3
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-04 09:42:19 +0300

    Feat : Added file structure - This adds the files in place to run the calculator

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2) {
+    return number1 + number2
+    };
+    var number1 = parseInt(prompt("Enter a number :"));
+    var number2 = parseInt(prompt("Enter another number :"));
+    alert("The answer is" + " " + add(number1, number2));
