01bc813744cef552f4e77c9895e688760de4d82c
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-10 08:25:28 +0300

    Feat ; Added functionality - This adds the form functonality including adding numbers

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@
 //     var number1 = parseInt(prompt("Enter a number :"));
 //     var number2 = parseInt(prompt("Enter another number :"));
 //     alert("The answer is" + " " + add(number1, number2));
+
+// BUSINESS (OR BACK-END) LOGIC ; 
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
+// USER INTERFACE/ (front-end logic);
+
+$(document).ready(function() {
+    $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+    });
+});
