b3aeee56ef2dd87b82d5749d67452f8fb94d3fc0
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-10 08:27:49 +0300

    Feat ; Result display - This makes an alert for result display

--- js/script.js
+++ js/script.js
@@ -26,6 +26,6 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    alert(add(number1, number2));
+    alert("Your answer is" + " " + add(number1, number2));
     });
 });
